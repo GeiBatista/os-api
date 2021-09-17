8a327af329f4ab690bd2a3e51ffd00736cc020f6
Gei Batista Santos <gei.santos@larcopetroleo.com.br>
2021-09-17 11:57:10 -0300

    Carga de dados p cliente

--- src/main/java/br/com/geibatisrta/os/services/DBService.java
+++ src/main/java/br/com/geibatisrta/os/services/DBService.java
@@ -46,14 +46,34 @@ public class DBService {
 		Tecnico t18 = new Tecnico(null, "Allana Raimunda Elaine Vieira", "439.899.940-00", "(75) 99833-8166");
 		Tecnico t19 = new Tecnico(null, "Ian Nathan da Paz", "274.452.930-35", "(85) 99178-0938");
 		Tecnico t20 = new Tecnico(null, "Bárbara Jaqueline Costa", "145.038.270-30", "(92) 99317-5199");
+		
 		Cliente c1 = new Cliente(null, "Joana Rosa Mirella Oliveira", "014.808.890-24", "(83) 98697-3849");
+		Cliente c2 = new Cliente(null, "Nathan Caio Enrico Alves", "952.878.422-44", "(75) 99937-2191");
+		Cliente c3 = new Cliente(null, "Cauã Roberto da Cruz", "242.972.827-39", "(83) 98556-9883");
+		Cliente c4 = new Cliente(null, "Levi Mateus Augusto Fernandes", "922.714.040-96", "(51) 99541-8027");
+		Cliente c5 = new Cliente(null, "Esther Ayla Emanuelly Castro", "980.720.487-91", "(71) 99614-7022");
+		Cliente c6 = new Cliente(null, "Julia Sophie Carolina Drumond", "673.127.591-30", "(86) 98852-0050");
+		Cliente c7 = new Cliente(null, "Raimundo Ricardo Teixeira", "454.761.277-39", "(79) 98573-5841");
+		Cliente c8 = new Cliente(null, "Nathan Anthony Matheus Assunção", "765.879.052-42", "(84) 98940-7773");
+		Cliente c9 = new Cliente(null, "Luzia Antonella Rodrigues", "102.692.643-29", "(92) 99350-5282");
+		Cliente c10 = new Cliente(null, "Sophia Analu Barbosa", "385.523.982-73", "(65) 99788-2212");
+		Cliente c11 = new Cliente(null, "Giovanna Débora Carla Rocha", "112.635.109-14", "(28) 55464-2062");
+		Cliente c12 = new Cliente(null, "Emily Sandra Jesus", "316.965.529-90", "(37) 14729-4247");
+		Cliente c13 = new Cliente(null, "Cauê Mateus Rodrigues", "231.376.348-06", "(53) 52432-8639");
+		Cliente c14 = new Cliente(null, "Augusto Luiz da Paz", "829.031.436-17", "(75) 20404-3589");
+		Cliente c15 = new Cliente(null, "Sueli Sebastiana Oliveira", "934.065.041-75", "(41) 18757-6339");
+		Cliente c16 = new Cliente(null, "Antônia Jaqueline Fernandes", "234.392.623-91", "(27) 47428-5537");
+		Cliente c17 = new Cliente(null, "Mariah Mariane Oliveira", "510.837.031-88", "(77) 28044-2356");
+		Cliente c18 = new Cliente(null, "Márcia Yasmin Teresinha Ramos", "437.750.738-92", "(83) 29283-3244");
+		Cliente c19 = new Cliente(null, "Agatha Julia Cristiane Gonçalves", "771.383.575-08", "(36) 25520-3681");
+		Cliente c20 = new Cliente(null, "Vitor Martin Isaac Aragão", "399.602.861-00", "(60) 64307-9924");
 		OS os1 = new OS(null, Prioridade.ALTA, "Teste OS", Status.ANDAMENTO, t1, c1);
 
 		t1.getList().add(os1);
 		c1.getList().add(os1);
 
 		tecnicoRepository.saveAll(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20));
-		clienteRepository.saveAll(Arrays.asList(c1));
+		clienteRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20));
 		osRepository.saveAll(Arrays.asList(os1));
 	}
 }
